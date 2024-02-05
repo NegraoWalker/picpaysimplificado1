b103406db156971fe824898da4929751c47004bd
Walker Negrão <c0662240@vale.com>
2024-02-05 14:06:07 -0300

    Repositório criado

---
+++ src/main/java/com/walker/picpaysimplificado1/Picpaysimplificado1Application.java
@@ -0,0 +1,13 @@
+package com.walker.picpaysimplificado1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Picpaysimplificado1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Picpaysimplificado1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/picpaysimplificado1/Picpaysimplificado1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.picpaysimplificado1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Picpaysimplificado1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
